b2e254847105ba81e8094e4ab0ac1b2c7f723dee
Anshika <pthkanshika@gmail.com>
2026-05-18 19:39:53 +0530

    Upgrade Next.js to fix security vulnerability

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -44,7 +44,7 @@ export function HeroSection() {
 
   {/* Darshan Timings Button */}
   <button
-  onClick={() => router.push("/darshan-timings")}
+  onClick={() => router.push("/darshan-timing")}
   className="
     group w-full sm:w-auto
     px-10 py-4
