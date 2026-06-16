3104f7d0436ab5193c8c749e2d64eeb865c11899
Anshika <pthkanshika@gmail.com>
2026-06-17 01:18:06 +0530

    Upgrade Next.js to fix security vulnerability

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export function HeroSection() {
   const router = useRouter(); // ✅ router yahin
 
   return (
- <section className="relative w-full h-[95vh] flex items-center justify-center overflow-hidden">   
+ <section className="relative w-full h-[90vh] flex items-center justify-center overflow-hidden">   
       {/* Background */}
     {/* Desktop Hero */}
 <img
