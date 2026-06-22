1af11ca9d617a877e448959d268ae590edc55a9c
Anshika <pthkanshika@gmail.com>
2026-06-22 14:29:32 +0530

    Upgrade Next.js to fix security vulnerability

--- app/components/TempleFooter.jsx
+++ app/components/TempleFooter.jsx
@@ -62,7 +62,7 @@ export function TempleFooter() {
         {/* Divider */}
       
 
-       <div className="border-t border-white/10 mt-6 pt-3 text-center">
+       <div className="border-t border-white/10 mt-4 pt-3 text-center">
 
   <p className="text-xs text-white/50">
     Website Designed & Developed by
